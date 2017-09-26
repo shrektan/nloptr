8f2e7f53449c0418fe0e95e7c3295e40514e2589
shrektan <shrektan@126.com>
2017-09-27 01:08:35 +0800

    test codes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,16 @@
 
 using namespace Rcpp;
 
+// test2
+double test2();
+RcppExport SEXP _nloptr_test2() {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    rcpp_result_gen = Rcpp::wrap(test2());
+    return rcpp_result_gen;
+END_RCPP
+}
 // test
 int test(int a);
 RcppExport SEXP _nloptr_test(SEXP aSEXP) {
@@ -20,6 +30,7 @@ END_RCPP
 RcppExport SEXP NLoptR_Optimize(SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_nloptr_test2", (DL_FUNC) &_nloptr_test2, 0},
     {"_nloptr_test", (DL_FUNC) &_nloptr_test, 1},
     {"NLoptR_Optimize", (DL_FUNC) &NLoptR_Optimize, 1},
     {NULL, NULL, 0}
